ca12f4cbf26ce671dccc7a3f4b83d35ce699e702
hfst.co <m@hfst.co>
2026-07-21 18:50:51 +0200

    Removed webtransport support for SCONE.

---
+++ dom/network/Scone.cpp
@@ -0,0 +1,123 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "Scone.h"
+
+#include "mozilla/Services.h"
+#include "mozilla/dom/ContentChild.h"
+#include "mozilla/dom/SconeBinding.h"
+#include "mozilla/net/SconeService.h"
+#include "mozilla/net/SocketProcessParent.h"
+#include "nsIObserverService.h"
+#include "nsIXULRuntime.h"
+
+namespace mozilla::dom::network {
+
+NS_IMPL_CYCLE_COLLECTION_CLASS(Scone)
+
+NS_IMPL_CYCLE_COLLECTION_UNLINK_BEGIN_INHERITED(Scone, DOMEventTargetHelper)
+  tmp->Shutdown();
+  NS_IMPL_CYCLE_COLLECTION_UNLINK_WEAK_REFERENCE
+NS_IMPL_CYCLE_COLLECTION_UNLINK_END
+
+NS_IMPL_CYCLE_COLLECTION_TRAVERSE_BEGIN_INHERITED(Scone, DOMEventTargetHelper)
+NS_IMPL_CYCLE_COLLECTION_TRAVERSE_END
+
+NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION(Scone)
+  NS_INTERFACE_MAP_ENTRY(nsIObserver)
+  NS_INTERFACE_MAP_ENTRY(nsISupportsWeakReference)
+NS_INTERFACE_MAP_END_INHERITING(DOMEventTargetHelper)
+
+NS_IMPL_ADDREF_INHERITED(Scone, DOMEventTargetHelper)
+NS_IMPL_RELEASE_INHERITED(Scone, DOMEventTargetHelper)
+
+Scone::Scone(nsPIDOMWindowInner* aWindow) : DOMEventTargetHelper(aWindow) {
+  nsCOMPtr<nsIObserverService> observerService =
+      mozilla::services::GetObserverService();
+  if (observerService) {
+    observerService->AddObserver(this, net::kSconeThroughputAdviceChangedTopic,
+                                 true);
+  }
+  RequestCurrentAdvice();
+}
+
+Scone::~Scone() { Shutdown(); }
+
+JSObject* Scone::WrapObject(JSContext* aCx, JS::Handle<JSObject*> aGivenProto) {
+  return Scone_Binding::Wrap(aCx, this, aGivenProto);
+}
+
+Nullable<uint64_t> Scone::GetThroughputAdvice() const {
+  Nullable<uint64_t> result;
+  if (mThroughputAdvice) {
+    result.SetValue(mThroughputAdvice.value());
+  }
+  return result;
+}
+
+void Scone::Shutdown() {
+  if (mShutdown) {
+    return;
+  }
+  mShutdown = true;
+
+  nsCOMPtr<nsIObserverService> observerService =
+      mozilla::services::GetObserverService();
+  if (observerService) {
+    observerService->RemoveObserver(this,
+                                    net::kSconeThroughputAdviceChangedTopic);
+  }
+}
+
+void Scone::DisconnectFromOwner() {
+  Shutdown();
+  DOMEventTargetHelper::DisconnectFromOwner();
+}
+
+NS_IMETHODIMP Scone::Observe(nsISupports* aSubject, const char* aTopic,
+                             const char16_t* aData) {
+  if (!strcmp(aTopic, net::kSconeThroughputAdviceChangedTopic)) {
+    Update(net::GetGlobalSconeThroughputAdvice(), true);
+  }
+  return NS_OK;
+}
+
+void Scone::RequestCurrentAdvice() {
+  auto resolve = [self = RefPtr{this}](Maybe<uint64_t> aAdvice) {
+    self->Update(aAdvice, true);
+  };
+  auto reject = [](mozilla::ipc::ResponseRejectReason) {};
+
+  if (XRE_IsContentProcess()) {
+    if (ContentChild* contentChild = ContentChild::GetSingleton()) {
+      contentChild->SendGetSconeThroughputAdvice()->Then(
+          GetCurrentSerialEventTarget(), __func__, std::move(resolve),
+          std::move(reject));
+    }
+    return;
+  }
+
+  if (RefPtr<net::SocketProcessParent> socketParent =
+          net::SocketProcessParent::GetSingleton()) {
+    socketParent->SendGetSconeThroughputAdvice()->Then(
+        GetCurrentSerialEventTarget(), __func__, std::move(resolve),
+        std::move(reject));
+    return;
+  }
+
+  resolve(net::GetGlobalSconeThroughputAdvice());
+}
+
+void Scone::Update(Maybe<uint64_t> aAdvice, bool aNotify) {
+  if (mShutdown || mThroughputAdvice == aAdvice) {
+    return;
+  }
+
+  mThroughputAdvice = aAdvice;
+  if (aNotify) {
+    DispatchTrustedEvent(u"change"_ns);
+  }
+}
+
+}  // namespace mozilla::dom::network
---
+++ dom/network/Scone.h
@@ -0,0 +1,48 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_dom_network_Scone_h
+#define mozilla_dom_network_Scone_h
+
+#include "mozilla/DOMEventTargetHelper.h"
+#include "mozilla/Maybe.h"
+#include "mozilla/dom/Nullable.h"
+#include "nsIObserver.h"
+#include "nsWeakReference.h"
+
+namespace mozilla::dom::network {
+
+class Scone final : public DOMEventTargetHelper,
+                    public nsIObserver,
+                    public nsSupportsWeakReference {
+ public:
+  NS_DECL_ISUPPORTS_INHERITED
+  NS_DECL_CYCLE_COLLECTION_CLASS_INHERITED(Scone, DOMEventTargetHelper)
+  NS_DECL_NSIOBSERVER
+
+  explicit Scone(nsPIDOMWindowInner* aWindow);
+
+  JSObject* WrapObject(JSContext* aCx,
+                       JS::Handle<JSObject*> aGivenProto) override;
+
+  Nullable<uint64_t> GetThroughputAdvice() const;
+
+  IMPL_EVENT_HANDLER(change)
+
+  void Shutdown();
+  void DisconnectFromOwner() override;
+
+ private:
+  ~Scone();
+
+  void RequestCurrentAdvice();
+  void Update(Maybe<uint64_t> aAdvice, bool aNotify);
+
+  Maybe<uint64_t> mThroughputAdvice;
+  bool mShutdown = false;
+};
+
+}  // namespace mozilla::dom::network
+
+#endif  // mozilla_dom_network_Scone_h
